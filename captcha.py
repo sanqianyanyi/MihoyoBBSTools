a881c805244c2d128eb3a040638482770ec1761e
sanqianyanyi <71774177+sanqianyanyi@users.noreply.github.com>
2023-07-22 12:51:29 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -2,16 +2,25 @@ from request import http
 
 
 def game_captcha(gt: str, challenge: str):
-	rep = http.post{
+	rep = http.post(
 	url="http://api.rrocr.com/api/recognize.html" , # API地址
-	data=
-		"appkey": "" ,
+	data={
+		"appkey": "b5432cd7ebdc4498b16aede06be48646" ,
 		"gt": gt,
 		"challenge" : challenge,
 		"referer" : "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign"
 		}
-    return rep["data"]["validate"]  # 失败返回None 成功返回validate
-
+	).json()
+	return rep["data"]["validate"]
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+	rep = http.post(
+	url="http://api.rrocr.com/api/recognize.html" , # API地址
+	data={
+		"appkey": "b5432cd7ebdc4498b16aede06be48646" ,
+		"gt": gt,
+		"challenge" : challenge,
+		"referer" : "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign"
+		}
+	).json()
+	return rep["data"]["validate"]
